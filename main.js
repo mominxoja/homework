caf197ecc75216d979487e77dcc002a6af16c880
mominxoja935615636gmail.com <mominxoja935615636@gmail.com>
2022-09-14 14:30:11 +0500

    main

---
+++ main.js
@@ -0,0 +1,276 @@
+//1
+
+
+// var son = 3
+// if(son > 0){
+//     console.log("son musbat");
+// }else{
+//     console.log("son manfiy")
+// }
+
+//2;3
+
+
+// var son = 105
+// if(son % 2 == 0){
+// console.log("son juft")
+// }if(son % 2 ==1){
+//     console.log("son toq")
+// }
+
+
+//4
+
+
+// var son = 2
+// var son = 4
+// if(son > 0){
+//     console.log("ikkala son musbat")
+
+// }if(son < 0){
+//     console.log("bitta son manfiy")
+// }
+
+
+//5
+
+
+// var a = 12
+// var b = 33 
+// var c = 40
+// if(a>b&&b>c){
+//     console.log("a son eng katta");
+    
+// }else{
+//     console.log("c son eng katta")
+// }
+
+
+//6
+
+
+// var a = 12
+// var b = 33 
+// var c = 4
+// if(a<b&&b>c){
+//     console.log("b son ortada")
+// }
+
+
+//7
+
+// var a = 12
+// var b = 32
+// if(a %2 ==0&&b %2 ==0){
+//     console.log("ikkalasi juft");
+// }else if(a %2 ==1 && b %2 ==1){
+//     console.log("ikkalasi toq")
+// }
+
+
+//8
+
+// var a = 12
+// var b = 33 
+// var c = 40
+// var d = 22
+
+// if(a > 0 && b  > 0 && c >0 && d >0){
+// console.log("hammasi musbat");
+// }
+
+//10
+
+// var du =1
+// var se =2
+// var ch =3
+// var pa =4
+// var ju =5
+// var sh =6
+// var ya =7
+// if(du == 1 ||se == 1 || ch ==1 || pa ==1 ||ju ==1 ||sh ==1 ||ya ==1){
+//     console.log("monday");
+// }
+// if(du == 2 ||se == 2 || ch ==2 || pa ==2 ||ju ==2 ||sh ==2 ||ya ==2){
+//     console.log("tuesday");
+// }
+// if(du == 3 ||se == 3 || ch ==3 || pa ==3 ||ju ==3 ||sh ==3 ||ya ==3){
+//     console.log("wensday");
+// }
+// if(du == 4 ||se == 4 || ch ==4 || pa ==4 ||ju ==4 ||sh ==4 ||ya ==4){
+//     console.log("thirsday");
+// }
+// if(du == 5 ||se == 5 || ch ==5 || pa ==5 ||ju ==5 ||sh ==5 ||ya ==5){
+//     console.log("friday");
+// }
+// if(du == 6 ||se == 6 || ch == 6 || pa ==6 ||ju ==6 ||sh ==6 ||ya ==6){
+//     console.log("saturday");
+// }
+// if(du == 7 ||se == 7 || ch ==7 || pa ==7 ||ju ==7 ||sh ==7 ||ya ==7){
+//     console.log("sunday");
+// }
+
+
+//11
+
+// var mobiuz=97
+// var ucell=93
+// if(mobiuz==97){
+//     console.log("mobiuz")
+
+// }if(ucell==93){
+//     console.log("ucell")
+// }
+
+
+//12
+
+// var a=4
+// // var b=6
+// if(a>0){
+//     console.log(a+1)
+
+// }
+// if(a<0){
+//     console.log(a-1)
+// }
+
+
+//14
+
+// var a=20
+// var b=3
+// if(a>b){
+//     console.log("a son katta");
+// }if(b>a){
+//     console.log("b son katta")
+// }
+
+
+//15
+
+
+// var a = 3
+// var b = 6
+// var c = 9
+// if(a<c&&b<c){
+//     console.log("c son katta")
+
+// }if(a<b&&b>c){
+//     console.log("b son katta")
+
+// }if(a>b&&a>c){
+//     console.log("a son katta")
+// }
+
+
+
+//16
+
+
+// var a = 3
+// var b = 2
+// var c = 9
+// if(a>c&&b>c){
+//     console.log("c son kichik")
+
+// }if(a>b&&b<c){
+//     console.log("b son kichik")
+
+// }if(a<b&&a<c){
+//     console.log("a son kichik")
+// }
+
+
+//18
+
+
+// "baxoni kiriting"
+// var baxo=2
+
+// if(baxo==5){
+//     console.log("5mln")
+// }
+// if(baxo==4){
+//     console.log("4mln")
+// }
+// if(baxo==3){
+//     console.log("3mln")
+// }
+// if(baxo==2){
+//     console.log("2mln")
+// }
+// if(baxo==1){
+//     console.log("1mln")
+// }
+
+
+
+//19
+
+
+// var a= 3
+// var b= 5
+// if(a>0){
+//     console.log("bu son musbat");
+// }
+// if(b>0){
+//     console.log("bu son musbat");
+// }
+// if(a %2 ==0){
+//     console.log("bu son juft");
+// } else 
+// {
+//     console.log ("buson toq")}
+// if(b %2 ==0){
+//     console.log("bu son juft");
+// }else 
+// {
+//     console.log ("buson toq")}
+
+
+
+//20
+
+
+// var yil =2008
+// if(yil.includes=8  ){
+//     console.log("14yosh");
+// }
+
+//22
+
+
+// var a=2
+// var b=5
+
+// if(a %2 ==1 || b %2 ==1){
+//     console.log("son toq");
+// }
+
+
+//23
+
+
+// var a =3
+// var b=4
+// var c=5
+// if (a>0&&b>0&&c>0){
+//     console.log("hammasi musbat son");
+// }
+
+
+//24
+
+
+
+
+// var a =3
+// var b=4
+// var c=-5
+// if (a>0||b>0||c>0){
+//     console.log("hammasi musbat son");
+// }
+// if(a<0||b<0||c<0){
+//     console.log("biri manfiy son");
+// }
